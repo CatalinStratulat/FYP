167d6d8d4f385f6c2768562c3fe8fcf28824e718
CatalinStratulat <catalin.stratulat321@gmail.com>
2020-01-19 13:08:34 +0000

    updating with dual audio

--- app/src/main/java/com/example/fyp/pinEntry.java
+++ app/src/main/java/com/example/fyp/pinEntry.java
@@ -2,11 +2,15 @@ package com.example.fyp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +23,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,7 +36,15 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
     String pathToSave;
     Long startTime;
     ArrayList<String> TouchDetected;
-    MediaRecorder recorder;
+
+    private static final int RECORDER_SAMPLERATE = 48000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_STEREO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+    private int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    private int BytesPerElement = 2; // 2 bytes in 16bit format
 
     final int REQUEST_PERMISSION_CODE = 1000;
 
@@ -55,6 +68,7 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pin_entry);
+        setButtonHandlers();
 
         //the whole screen becomes sensitive to touch
         RelativeLayout mLinearLayoutMain = findViewById(R.id.relativeLayout);
@@ -65,40 +79,126 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
         }
         path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + UUID.randomUUID().toString();
         pathToSave = path + "_audio_record.3gp";
-        setupMediaRecorder();
 
-        if (CheckPermissionFromDevice())
-        {
-            try
-            {
-                recorder.prepare();
-                recorder.start();
-                startTime = System.currentTimeMillis();
-                System.out.println(startTime);
-                TouchDetected = new ArrayList<>();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-
-//            Toast.makeText(pinEntry.this, "Recording...", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            requestPermission();
-        }
-
-//        Toast.makeText(pinEntry.this, "Permission was granted", Toast.LENGTH_SHORT).show();
+        findViewById(R.id.btnStart).performClick();
     }
 
-    private void setupMediaRecorder() {
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioSamplingRate(44000);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile(pathToSave);
+    private void setButtonHandlers() {
+        (findViewById(R.id.btnStart)).setOnClickListener(btnClick);
+        (findViewById(R.id.btnStop)).setOnClickListener(btnClick);
+    }
+
+    private void enableButton(int id, boolean isEnable) {
+        (findViewById(id)).setEnabled(isEnable);
+    }
+
+    private void enableButtons(boolean isRecording) {
+        enableButton(R.id.btnStart, !isRecording);
+        enableButton(R.id.btnStop, isRecording);
+    }
+
+    private void startRecording() {
+
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+        startTime = System.currentTimeMillis();
+        System.out.println(startTime);
+        TouchDetected = new ArrayList<>();
+    }
+
+    //convert short to byte
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudioDataToFile() {
+        // Write the output audio in byte
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + UUID.randomUUID().toString();
+        String pathToSave = path + "_audio_record.pcm";
+//        String filePath = "/sdcard/voice8K16bitmono.pcm";
+        short sData[] = new short[BufferElements2Rec];
+
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(pathToSave);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            recorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            try {
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+                byte bData[] = short2byte(sData);
+                os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+            System.exit(0);
+        }
+    }
+
+    private View.OnClickListener btnClick = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnStart: {
+                    enableButtons(true);
+                    startRecording();
+                    break;
+                }
+                case R.id.btnStop: {
+                    enableButtons(false);
+                    stopRecording();
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
@@ -150,7 +250,7 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
         TouchDetected.add(touch);
 
         if (textview3.length() == 4) {
-            if (pinNr < 10) {
+            if (pinNr < 2) {
 //                Toast.makeText(pinEntry.this, "" + textview3.getText() + " Has Been Entered", Toast.LENGTH_SHORT).show();
                 String figure = "";
                 textview3.setText(figure);
@@ -158,16 +258,15 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
                 String title = "Enter PIN [" + (pinList.get(pinNr)) + "]";
                 textview1.setText(title);
                 pinNr = pinNr + 1;
-            } else if (pinNr == 10) {
+            } else if (pinNr == 2) {
                 ArrayList<String> properList = ((Data) this.getApplication()).getList();
                 properList.add("audio_file_path" + "=" + pathToSave);
-                recorder.stop();
+//                findViewById(R.id.btnStop).performClick();
+
                 System.out.println(TouchDetected.toString());
                 processList(properList,TouchDetected);
 //                  generateNoteOnSD(output);
 //                Toast.makeText(pinEntry.this, "Thank you for completing this sample run", Toast.LENGTH_SHORT).show();
-                finish();
-                System.exit(0);
             }
 
         } else {
@@ -255,6 +354,7 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
 
     public void pinButton1(View view) {
         TextView textview = findViewById(R.id.textView3);
+        view.playSoundEffect(android.view.SoundEffectConstants.CLICK);
         if (textview.length() < 4) {
             String str = "1";
             textview.append(str);
@@ -360,48 +460,3 @@ public class pinEntry extends AppCompatActivity implements View.OnTouchListener 
     }
 }
 
-
-//        for(String item : properList)
-//        {
-//            int location = item.indexOf("=");
-//            if (location != -1)
-//            {
-//                String key = item.substring(0, location);
-//                String value = item.substring(1, location);
-//                Row row = sheet.createRow(properList.indexOf(item));
-//                row.createCell(properList.indexOf(item)).setCellValue(key);
-//            }
-//        }
-//class BooVariable {
-//    private int presses = 0;
-//    private ChangeListener listener;
-//
-//    public int getPResses() {
-//        return presses;
-//    }
-//
-//    public void increment() {
-//        presses = presses + 1;
-//        if (listener != null) listener.onChange();
-//    }
-//
-//    public void decrement() {
-//        if (presses > 0)
-//        {
-//            presses = presses - 1;
-//            if (listener != null) listener.onChange();
-//        }
-//    }
-//
-//    public ChangeListener getListener() {
-//        return listener;
-//    }
-//
-//    public void setListener(ChangeListener listener) {
-//        this.listener = listener;
-//    }
-//
-//    public interface ChangeListener {
-//        void onChange();
-//    }
-//}
